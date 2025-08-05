9e40dde32a9b93e4cf6d36341be4466fe3030201
@ochiengwilliam79 <ochiengwilliam79@gmail.com>
2025-08-05 10:20:08 +0300

    baseurl

--- src/App.tsx
+++ src/App.tsx
@@ -44,11 +44,8 @@ import SubscriptionPackage from "./components/Package/SubscriptionPackage";
 import Advertisement from "./components/Advertisement";
 import MobileUser from "./components/MobileUser";
 import BankSetup from "./components/Accounting/Bank Setup";
-<<<<<<< Updated upstream
 import BranchSetup from "./components/Accounting/Branch";
-=======
 import Company from "./components/Accounting/Company";
->>>>>>> Stashed changes
 
 export default function App() {
   return (
